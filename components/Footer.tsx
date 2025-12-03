85a63cfd006884802b363c9565bf60db9654b040
Subrata <subrata@ascinatetech.com>
2025-12-03 19:30:03 +0530

    i change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
                  <div className="grid grid-cols-4 justify-between items-start gap-4">
                     <div className="comonus-crads">
                          <h5> About Niche </h5>
-                         <p className="mt-3"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard. </p>
+                         <p className="mt-3"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard. </p>
                          <ul className="mt-5 flex items-center">
                             <li>
                               <Link href="/" className="list01"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(255,255,255,1)"><path d="M14 13.5H16.5L17.5 9.5H14V7.5C14 6.47062 14 5.5 16 5.5H17.5V2.1401C17.1743 2.09685 15.943 2 14.6429 2C11.9284 2 10 3.65686 10 6.69971V9.5H7V13.5H10V22H14V13.5Z"></path></svg> </Link>
@@ -66,9 +66,7 @@ export default function Footer() {
                          <h5> Top Countries </h5>
                          
                          <ul className="mt-5 c-list01">
-                            <li>
-                              <Link href="/"> Afghanistan </Link>
-                            </li>
+                           
                             <li>
                               <Link href="/"> Malaysia </Link>
                             </li>
