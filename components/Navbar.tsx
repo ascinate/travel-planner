a10ec550f98c2b08d99cd2e0e34bc9ac607f9e91
Subrata <subrata@ascinatetech.com>
2025-12-03 15:44:37 +0530

    navbar edit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
